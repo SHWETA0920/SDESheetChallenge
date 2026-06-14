1f5d7ac2ccbdbcd766e1f869c400f53919637597
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-06-14 11:53:12 +0530

    Create Remove Duplicate from Sorted array.cpp

---
+++ DAY14/Remove Duplicate from Sorted array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        unordered_set<int>s;
+        int x=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]!=nums[i-1]){                
+                nums[x]=nums[i];
+                x++;
+            }
+        }
+        return x;
+        
+    }
+};
