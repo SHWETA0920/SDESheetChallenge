0109e18d4a577a116f6fff20d41147302325b197
Shweta Gupta <162487913+SHWETA0920@users.noreply.github.com>
2026-06-27 16:22:36 +0530

    Create 239. Sliding Window Maximum.cpp

---
+++ DAY27/239. Sliding Window Maximum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        int n=nums.size();
+        deque<int>deq;
+        vector<int>v;
+        for(int i=0;i<n;i++){
+            while(!deq.empty() && deq.front()<=i-k){
+                deq.pop_front();
+            }
+            while(!deq.empty() && nums[i]>nums[deq.back()]){
+                deq.pop_back();
+            }
+            deq.push_back(i);
+
+            if(i>=k-1){
+                v.push_back(nums[deq.front()]);
+            }
+        }
+        return v;
+        
+    }
+};
